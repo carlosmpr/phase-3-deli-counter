37cf06dead5ab474a11d39b771c788513742ac31
Carlos Polanco <carlospolanco@Carloss-Mac-mini.local>
2021-06-29 10:57:52 -0400

    commit tree 1386e5c535e5cdf6e106f0c35ea0a2df6c06cac0

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,8 @@
 # Write your code here.
+def line (array)
+    if array.size == 0
+        puts "The line is currently empty."
+    end
+    
+    array.each{| person , index| puts("#{index}. #{person} ") }
+end
